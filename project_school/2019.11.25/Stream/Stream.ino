b084e9a92e5d10114002d1b7dad3e924cac1c8ce
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-11-25 15:27:26 +0900

    2019.11.25
    완성본

---
+++ project_school/2019.11.25/Stream/Stream.ino
@@ -0,0 +1,193 @@
+
+
+#include <SoftwareSerial.h>//와이파이 통신
+
+#include "FirebaseESP8266.h" //파이어베이스 사용
+#include <ESP8266WiFi.h> //와이파이 사용
+
+#include <string.h>
+
+//와이파이와 파이어베이스 연동
+#define FIREBASE_HOST "arduinowebserver2.firebaseio.com" 
+#define FIREBASE_AUTH "cg9MKZYgyzWQPuwsI3WnXXaUJgqp4DGLlirr8048" 
+#define WIFI_SSID "KBU" 
+#define WIFI_PASSWORD "1952bible!"
+
+int count_check = 0;//파이어베이스에 비교하기위한 기존 할일 값을 한번만 저장
+
+  unsigned long pre_time = 0;
+  unsigned long cur_time = 0;
+  
+//Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+FirebaseJson json;
+
+SoftwareSerial s(D6,D5); // (Rx, Tx) //와이파이 통신
+
+
+void printResult(FirebaseData &data);
+
+void setup()
+{
+
+  Serial.begin(9600);
+  s.begin(9600); 
+  //와이파이 연결
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+ //파이어베이스 연결
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  //와이파이와 아두이노 연결 : https://www.youtube.com/watch?v=SiU-QZwik8w
+  /*
+  This option allows get and delete functions (PUT and DELETE HTTP requests) works for device connected behind the
+  Firewall that allows only GET and POST requests.
+  
+  Firebase.enableClassicRequest(firebaseData, true);
+  */
+
+  String path = "/testBucket/WIFIControl";
+  String path2 = "/testBucket/WIFIDoIt";
+
+  Serial.println("------------------------------------");
+  Serial.println("Get double test...");
+  
+  
+    //Also can use Firebase.get instead of Firebase.setInt
+    if (Firebase.getString(firebaseData, path))
+    {
+      Serial.print("WIFIControl VALUE: ");
+      printResult(firebaseData);
+      Serial.println("------------------------------------");
+      Serial.println();
+      
+    }
+    if (Firebase.getString(firebaseData, path2))
+    {
+      Serial.print("WIFIControl VALUE: ");
+      printResult2(firebaseData);
+      Serial.println("------------------------------------");
+      Serial.println();
+      
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+
+
+}
+String data_checks = ""; //와이파이 값 저장
+void printResult2(FirebaseData &data)
+{
+    if (data.dataType() == "int")
+        Serial.println(data.intData());
+    else if (data.dataType() == "float")
+        Serial.println(data.floatData(), 5);
+    else if (data.dataType() == "double")
+        printf("%.9lf\n", data.doubleData());
+    else if (data.dataType() == "boolean")
+        Serial.println(data.boolData() == 1 ? "true" : "false");
+    else if (data.dataType() == "string")
+    {       
+
+            String aa = data.stringData();
+            aa.remove(0,2);
+            aa.remove(aa.length()-2, aa.length()-1);// "/제거
+            aa += ",";
+
+            if(count_check == 0)
+            {
+              data_checks += aa;
+              count_check = 1;
+              
+              char a[50];        
+              aa.toCharArray(a,50);
+              Serial.print("할일 : ");
+              Serial.println(a);
+              s.write(a);
+              
+            }
+            else{
+              if(data_checks == aa){
+                  Serial.println("특수문자");
+                }
+                else{
+                  Serial.println("초기화");
+                  count_check = 0;
+                  data_checks = "";
+                  }
+            }
+            Serial.print("값 뭐로 저장? : ");
+            Serial.println(data_checks);
+    delay(1000);
+    }
+    
+}
+void printResult(FirebaseData &data)
+{
+    if (data.dataType() == "int")
+        Serial.println(data.intData());
+    else if (data.dataType() == "float")
+        Serial.println(data.floatData(), 5);
+    else if (data.dataType() == "double")
+        printf("%.9lf\n", data.doubleData());
+    else if (data.dataType() == "boolean")
+        Serial.println(data.boolData() == 1 ? "true" : "false");
+    else if (data.dataType() == "string")
+    {        
+            int a;
+            a = data.stringData().toInt();
+            s.write(a);//a.toInt()
+            Serial.print("제어 : ");
+            Serial.println(a);
+    }
+    
+}
+
+void loop()
+{ 
+  String path = "/testBucket/WIFIControl";//경로
+  String path2 = "/testBucket/WIFIDoIt";
+  delay(5000);
+  if (Firebase.getString(firebaseData, path))
+    {
+      Serial.print("WIFIControl VALUE: ");
+      printResult(firebaseData);
+      Serial.println("------------------------------------");
+      Serial.println();   
+    }
+  if (Firebase.getString(firebaseData, path2))
+    {
+      Serial.print("WIFIControl VALUE: ");
+      printResult2(firebaseData);
+      Serial.println("------------------------------------");
+      Serial.println();
+      
+    }
+  else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+
+}
